6db7aff1e3a2b97ac3dc93d966740bc2f68dbfd6
seleejane <seleejane@gmail.com>
2024-11-14 15:56:46 +0200

    User Model Update

--- plastics/settings.py
+++ plastics/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authuser',
+    'core',
 ]
+
+AUTH_USER_MODEL = 'authuser.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
